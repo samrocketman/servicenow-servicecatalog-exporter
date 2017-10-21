9375de8f66b2535426a733bcd2a16973522d3105
Sam Gleske <sam.mxracer@gmail.com>
2017-10-21 00:59:12 -0700

    Additional queries for eventually the query builder

--- exporter.py
+++ exporter.py
@@ -8,8 +8,8 @@ password = os.environ.get('SNOW_PASS')
 instance = os.environ.get('SNOW_INSTANCE')
 
 
-#key name is table and value is key of that table where the associated sys_id is located
-tables_to_export = {
+# key name is table and value is key of that table where the associated sys_id is located
+tables = {
     'item_option_new': 'cat_item',
     'catalog_script_client': 'cat_item',
     'catalog_ui_policy': 'catalog_item',
@@ -36,22 +36,67 @@ tables_to_export = {
 s = pysnow.Client(instance=instance, user=user, password=password)
 request = s.query(table='sc_cat_item', query={})
 
-#export only one item (for testing purposes)
+# export only one item (for testing purposes)
 record = request.get_multiple(order_by=['-created-on']).next()
 sys_id = record['sys_id']
 export = {'sc_cat_item': [record]}
 
-for table_name, sysid_key in tables_to_export.iteritems():
+# Name all the related lists (a.k.a. export related records from other tables)
+for table_name, sysid_key in tables.iteritems():
     try:
         query = s.query(table=table_name, query={sysid_key: sys_id})
         for record in query.get_multiple():
-            if record:
-                if not table_name in export:
-                    export[table_name] = []
-                export[table_name].append(record)
+            if not table_name in export:
+                export[table_name] = []
+            export[table_name].append(record)
     except NoResults:
-        #Query yielded no results so just ignore it
+        # Query yielded no results so just ignore it
         pass
 
-#only export tables which have records to export
-print json.dumps(dict((k, v) for k, v in export.iteritems() if v))
+catalogID = [] #done
+categoryID = [] #done
+vsrelID = []
+vsID = []
+vID = []
+qcID = [] #done
+uipID = []
+actID = [] #done
+csID = []
+
+# Query for Catalogs
+if 'sc_cat_item_catalog' in export:
+    for catalog in export['sc_cat_item_catalog']:
+        catalogID.append(catalog['sc_catalog']['sys_id'])
+
+# Query for Categories
+if 'sc_cat_item_category' in export:
+    for category in export['sc_cat_item_category']:
+        categoryID.append(category['sc_category']['sys_id'])
+
+# Query for variables to get question choices
+if 'item_option_new' in export:
+    for item in export['item_option_new']:
+        try:
+            # Query for question choices
+            query = s.query(table='question_choice', query={'question': item['sys_id']})
+            for record in query.get_multiple():
+                qcID.append(record['sys_id'])
+        except NoResults:
+            # Query yielded no results so just ignore it
+            pass
+
+# Query for ui catalog ui policies to get policy actions
+if 'catalog_ui_policy' in export:
+    for catpol in export['catalog_ui_policy']:
+        try:
+            # Query for ui policy actions
+            query = s.query(table='catalog_ui_policy_action', query={'ui_policy': item['sys_id']})
+            for record in query.get_multiple():
+                actID.append(record['sys_id'])
+        except NoResults:
+            # Query yielded no results so just ignore it
+            pass
+
+# Query for variable set relationships
+
+print json.dumps(export)
