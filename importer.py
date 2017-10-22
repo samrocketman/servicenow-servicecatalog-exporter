1e0d2911387be43f135c93dcd7bee32d7bcb0e6b
Sam Gleske <sam.mxracer@gmail.com>
2017-10-22 14:44:40 -0700

    importer now works to create a record

--- importer.py
+++ importer.py
@@ -16,6 +16,7 @@ Options:
   --instance=<instance>  The ServiceNow instance to export from.  Overrides the SNOW_INSTANCE environment variable.
 """
 
+from pysnow.exceptions import NoResults
 import json
 import os
 import pysnow
@@ -33,26 +34,39 @@ class Importer:
         self.snow = snow
 
 
+    def insert_or_update(self, table, record):
+        """
+        Try to update the record but if it doesn't exist then create it (insert).
+        """
+        try:
+            request = s.query(table=table, query={'sys_id': record['sys_id']})
+            #request.get_single()
+            response = request.update(record)
+        except NoResults:
+            # Record does not exist so create it
+            response = self.snow.insert(table=table, payload=record)
+        return response
+
+
     def write_multiple_records(self, table, records):
         """
         Writes multiple records (or a single record) to a ServiceNow instance.
         """
         payload = None
+        response = []
         if not isinstance(table, str):
             raise ValueError("table must be a str.")
 
         if isinstance(records, dict):
             # Single record
-            payload = records
+            response.append(self.insert_or_update(table, records))
 
         if isinstance(records, list):
             # Multiple records
-            payload = {"records": records}
+            for record in records:
+                response.append(self.insert_or_update(table, record))
 
-        if not payload:
-            raise ValueError("No records to send to service-now.")
-
-        self.snow.insert(table=table, payload=payload)
+        return response
 
 
 if __name__ == '__main__':
@@ -76,4 +90,4 @@ if __name__ == '__main__':
 
     for table, records in export.iteritems():
         print >> sys.stderr, 'Importing records to %s' % table
-        importer.write_multiple_records(str(table), records)
+        print json.dumps(importer.write_multiple_records(str(table), records), indent=4, separators=(',', ': '), sort_keys=True)
